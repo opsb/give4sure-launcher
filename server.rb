8039e8ae704871f2e07a8ab5fd382cc690c06162
opsb <oliver@opsb.co.uk>
2014-08-05 13:24:18 -0500

    Redirect to retailer's home page

--- server.rb
+++ server.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
-class Retailer < Struct.new(:id, :name, :touch_icon_url)
+class Retailer < Struct.new(:id, :name, :home_page_url, :touch_icon_url)
 	def self.find(id)
 		RETAILERS.find{|r|r.id == id}
 	end
 end
 
 RETAILERS = [
-	Retailer.new(0, "Marks and Spencer", "http://asset1.marksandspencer.com/is/image/mands/logo-mns?$CHECKOUT_LOGO$"),
-	Retailer.new(1, "Waitrose", "https://pbs.twimg.com/profile_images/463627280980254720/RvbRvneq.jpeg"),
-	Retailer.new(2, "Sainburys", "http://shakefox.com/images2.moneysavingexpert.com/images/deals/tag-images/sainsburys_logo_d.gif")
+	Retailer.new(0, "Marks and Spencer", "http://www.marksandspencer.com/", "http://asset1.marksandspencer.com/is/image/mands/logo-mns?$CHECKOUT_LOGO$"),
+	Retailer.new(1, "Waitrose", "http://www.waitrose.com/", "https://pbs.twimg.com/profile_images/463627280980254720/RvbRvneq.jpeg"),
+	Retailer.new(2, "Sainburys", "http://www.sainsburys.co.uk/", "http://shakefox.com/images2.moneysavingexpert.com/images/deals/tag-images/sainsburys_logo_d.gif")
 ]
 
 get '/' do
@@ -18,6 +18,7 @@ get '/' do
 end
 
 get '/retailers/:id' do
-	@retailer = Retailer.find(params['id'].to_i)
+	retailer = Retailer.find(params['id'].to_i)
+	redirect to(retailer.home_page_url)
 	erb :retailer
 end
